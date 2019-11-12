eb4fbd0657c5331878b2ec12d9d4286be8473a69
wissal488 <wissalajb@gmail.com>
2019-11-12 14:56:05 +0000

    jsx

---
+++ exemple0/ex0.js
@@ -0,0 +1,4 @@
+const para=React.createElement('p',null,"INTRODUCTION ON MIND UPLOADING");
+const h=React.createElement('h2',null,"Bonsoir");
+const app=React.createElement('div',{id:'div1'},h,para);
+ReactDOM.render(app,document.getElementById('root'));
